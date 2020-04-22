3320f91d81513b7d28e3aa55446aa6dba1846f43
IMRAN <mdimranhossaincsebd@gmail.com>
2020-04-23 03:46:53 +0600

    Part1

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots=[
+
+{
+    id:1,
+    name:'Lear Gro ',
+    username:'LGRO',
+    email:'lgo@mail.com'
+
+
+},
+{
+    id:2,
+    name:'Ervin How ',
+    username:'ERVH',
+    email:'ervh@mail.com'
+},
+{
+    id:3,
+    name:'Clemen Boa ',
+    username:'Clboa',
+    email:'Clboa@mail.com'
+
+
+},
+{
+    id:4,
+    name:'Patric Lebs ',
+    username:'Patlrb',
+    email:'Patlrb@mail.com'
+
+
+},
+{
+    id:5,
+    name:'Chersa Die ',
+    username:'Chdie',
+    email:'Chdie@mail.com'
+
+},
+
+
+]
+
+
